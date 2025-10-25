20ba2726bf5c04599048b5dcbd06211a8edbf256
Hrudnanda <154302062+Hrudnanda@users.noreply.github.com>
2025-10-25 17:41:25 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+// Import your favicon image from src/assets
+import me12 from './assets/me12.jpg';
+
+// Dynamically set favicon
+const favicon = document.getElementById('dynamic-favicon');
+if (favicon) {
+  favicon.href = me12;
+}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <App />
+    </StrictMode>
   </BrowserRouter>
-)
+);
